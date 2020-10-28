9feb26a547d4582b7819026ce960fbf24a6ff3db
igaozp <igaozp@hotmail.com>
2020-10-28 21:57:21 +0800

    Feat: Add enter key event listener.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 function search() {
   let searchContent = document.getElementById('search-input').value;
   let url = "https://www.google.com/search?q=" + decodeURIComponent(searchContent);
-  window.open(url)
+  window.open(url, "_self");
 }
 
-document.getElementById('search-button').addEventListener('click', search, false)
+document.getElementById('search-button').addEventListener('click', search, false);
+
+document.getElementById('search-input').addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    search();
+  }
+})
