5b8be110bea019e6016f501027dfb86dd78cfbe5
igaozp <igaozp@hotmail.com>
2020-11-07 11:04:08 +0800

    Feat: Add like button click event

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-function search() {
+document.getElementById('search-button').addEventListener('click', () => {
   let searchContent = document.getElementById('search-input').value;
-  let url = "https://www.google.com/search?q=" + decodeURIComponent(searchContent);
-  window.open(url, "_self");
-}
-
-document.getElementById('search-button').addEventListener('click', search, false);
+  let url = 'https://www.google.com/search?q=' + decodeURIComponent(searchContent);
+  window.open(url, '_self');
+}, false);
 
 document.getElementById('search-input').addEventListener('keyup', (event) => {
   if (event.key === 'Enter') {
-    search();
+    let searchContent = document.getElementById('search-input').value;
+    let url = 'https://www.google.com/search?q=' + decodeURIComponent(searchContent);
+    window.open(url, '_self');
   }
 })
+
+document.getElementById('like-button').addEventListener('click', () => {
+  window.open('https://github.com/futurefog/new-page', '_self')
+}, false);
