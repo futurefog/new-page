870a2aa50e90f79a49e6b756f11381e7b6903e0a
igaozp <igaozp@hotmail.com>
2020-12-28 22:07:27 +0800

    Feat: Refactor main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
-document.getElementById('search-button').addEventListener('click', () => {
+function search() {
   let searchContent = document.getElementById('search-input').value;
   let url = 'https://www.google.com/search?q=' + decodeURIComponent(searchContent);
+  openUrl(url);
+}
+
+function openUrl(url) {
   window.open(url, '_self');
+}
+
+document.getElementById('search-button').addEventListener('click', () => {
+  search();
 }, false);
+
 
 document.getElementById('search-input').addEventListener('keyup', (event) => {
   if (event.key === 'Enter') {
-    let searchContent = document.getElementById('search-input').value;
-    let url = 'https://www.google.com/search?q=' + decodeURIComponent(searchContent);
-    window.open(url, '_self');
+    search();
   }
 })
 
 document.getElementById('like-button').addEventListener('click', () => {
-  window.open('https://github.com/futurefog/new-page', '_self')
+  openUrl('https://github.com/futurefog/new-page');
 }, false);
